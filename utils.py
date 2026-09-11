4a7a0e4e4c2de166e02c43f39db51ff02b79c8a4
vzunz <violette.zunz@uclouvain.be>
2026-09-11 13:21:27 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -297,11 +297,12 @@ def display_profile(researcher, subdomain_map):
     pole = researcher['pole']
     color = pole_colors.get(pole, "#777777")
 
-    st.markdown(
-        f"### {researcher['prenom']} {researcher['nom']}&nbsp;&nbsp;&nbsp;&nbsp;"
-        f':color[{pole}]{{foreground="white" background={color}}}',
-        text_alignment="left"
-    )
+    st.container(border=True):
+        st.markdown(
+            f"### {researcher['prenom']} {researcher['nom']}&nbsp;&nbsp;&nbsp;&nbsp;"
+            f':color[{pole}]{{foreground="white" background={color}}}',
+            text_alignment="left"
+        )
 
     st.subheader("Research areas")
     
